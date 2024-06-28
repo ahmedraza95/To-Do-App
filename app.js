235db7ccffc2b1123489bf2a5466156b334c36dc
ahmedraza95 <ahmedhome6789@gmail.com>
2024-06-28 16:00:45 +0500

    fourth

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let card = document.querySelectorAll(".card");
+let fullPart = document.querySelector(".card1")
 let btn = document.querySelector(".button");
 let main = document.querySelector(".card1");
+let form = document.querySelector(".form");
 
+
+console.log("he");
 let savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
 
 if (!savedTasks) {
@@ -9,67 +13,91 @@ if (!savedTasks) {
 }
 
 
-const addTask = (e) => {
-    e.preventDefault();
 
+const addTask = (e) => {
+    e.preventDefault();     
+    
     const currentForm = e.target; // current form element
-    const value = currentForm.elements[0].value; // value written in form's input
+    const value = currentForm.elements[0].value;
+    const valueTextArea = currentForm.elements[1].value;
     const parent = currentForm.parentElement; // parent of form i.e div.column
-    const ticket = newElement(value);
+    // const ticket = newElement(value);
+    createCard(value,valueTextArea)
+    
+    
     if (!value) {
         
         return;
-    
-        } 
-    parent.insertBefore(ticket, parent.secondChild)
-    // parent.insertBefore(ticket, parent.firstChild); // adding new task before the form
-    console.log(value);
-           const h3Value = parent.children[0].innerText;
-
-
-    currentForm.reset(); // clearing form
-
-    if (!Array.isArray(savedTasks[h3Value])) {
-        savedTasks[h3Value] = [];
+        
     }
-    savedTasks[h3Value].push(value);
-    localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
-
-    // console.log(savedTasks.Card1[1]);
-
-for (let i = 0; i < savedTasks.length; i++) {
-        console.log(savedTasks.Card1[i]);
-}
-};
-for (let i = 0; i < card.length; i++) {
-    const form = card[i].children[1]; // selecting every column's form because form is last element
-
+    // for (const test in savedTasks) {
+        //     let div = createCard(test)
+        //     main.appendChild(div)
+        // }
+        // parent.insertBefore(ticket, parent.secondChild)
+        // parent.insertBefore(ticket, parent.firstChild); // adding new task before the form
+        const h3Value = "";
+        
+        
+        currentForm.reset(); // clearing form
+    };
+// for (let i = 0; i < card.length; i++) {
+    //     const form = card[i].children[1]; // selecting every column's form because form is last element
+    
+    // }
     form.addEventListener("submit", addTask);
-}
+    
+    // let newElement = (value) => {
+        //     let ticket = document.createElement("p");
+        //     let createText = document.createTextNode(value);
+        //     ticket.appendChild(createText);
+        //     return ticket
+// }
 
-let newElement = (value) => {
-    let ticket = document.createElement("p");
-    let createText = document.createTextNode(value);
-    ticket.appendChild(createText);
-    return ticket
-}
 
-let createCard = () => {
+
+
+let createCard = (value, valueTextArea) => {
     const newCard = document.createElement("div");
     newCard.setAttribute("class", "card")
     const newHeading = document.createElement("h2");
-    const headingVAlue = document.createTextNode("Hello")
-
+    const headingVAlue = document.createTextNode(value)
     newHeading.appendChild(headingVAlue);
-    console.log(newHeading);
-    const newForm = document.createElement("form");
-    const newInput = document.createElement("input");
-    newInput.setAttribute("type","text");
-    newInput.setAttribute("placeholder","Add Task");
-    newForm.appendChild(newInput);
-    console.log(newForm);
+    
+    const para = document.createElement("p");
+    const paraValue = document.createTextNode(valueTextArea);
+    para.appendChild(paraValue);
+    
+    newCard.appendChild(newHeading);
+    newCard.appendChild(para);
+    main.appendChild(newCard);
+    
+    
+    
+    const h3Value = newHeading.innerText;
+    
+    if (!Array.isArray(savedTasks[h3Value])) {
+        savedTasks[h3Value] = [];
+    }
+    savedTasks[h3Value].push(valueTextArea);
+    localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
+    
+    
+    for (let i = 0; i < savedTasks.length; i++) {
+        console.log(savedTasks.Card1[i]);
+    }
+    savedTasks[h3Value].push(addTask);
+    
+} 
+for (const test in savedTasks) {
+    let showValue = savedTasks[test][0];
+    let showHeading = test;
 
+    createCard(showHeading, showValue);
 
 }
 
-// createCard()
+
+// creteFunction()
+
+// console.log(createCard());
