0e7e3396752380fe14805923c4f2d3881e6ec158
ahmedraza95 <ahmedhome6789@gmail.com>
2024-07-04 01:14:44 +0500

    last

--- app.js
+++ app.js
@@ -18,10 +18,9 @@ const addTask = (e) => {
 
     const currentForm = e.target;
     const value = currentForm.elements[0].value;
-    const valueTextArea = currentForm.elements[1].value;
     const parent = currentForm.parentElment;
 
-    createCard(value, valueTextArea)
+    createCard(value)
 
 
 
@@ -35,7 +34,32 @@ form.addEventListener("submit", addTask);
 
 
 
-let createCard = (value, valueTextArea) => {
+let createTicket = (e) => {
+    e.preventDefault();
+    let targetDiv = e.target.parentElement.parentElement;
+    let targetinput = e.target.parentElement.children[0];
+    let newPara = document.createElement("p")
+    newPara.textContent = targetinput.value;
+    console.log(targetinput.value);
+
+    let inputval = targetinput.value;
+    targetDiv.appendChild(newPara);
+    let getForm = targetinput.parentElement;
+    getForm.reset();
+    let getValueHeading = targetDiv.children[0].children[0].children[0];
+
+    
+    getValueHeading = getValueHeading.innerText;
+
+    savedTasks[getValueHeading].push(inputval);
+    console.log(targetinput);
+    localStorage.setItem("savedTasks" , JSON.stringify(savedTasks));
+    
+
+    return getValueHeading;
+}
+
+let createCard = (value) => {
     const newCard = document.createElement("div");
     newCard.setAttribute("class", "card")
 
@@ -68,9 +92,9 @@ let createCard = (value, valueTextArea) => {
     const newHeading = document.createElement("h2");
     const headingVAlue = document.createTextNode(value)
     newHeading.appendChild(headingVAlue);
-
-
     newDiv1.appendChild(newHeading);
+
+
 
     newDiv2.appendChild(UpdateDiv)
     newDiv2.appendChild(deleteBtn)
@@ -81,26 +105,40 @@ let createCard = (value, valueTextArea) => {
 
 
 
-    const para = document.createElement("p");
-    const paraValue = document.createTextNode(valueTextArea);
-    para.appendChild(paraValue);
+    let form = document.createElement("form");
+    form.setAttribute("class", "innerForm")
+    let input = document.createElement("input");
+    input.setAttribute("placeholder", "Add List")
+    input.setAttribute("type", "text");
+    input.setAttribute("class", "innerDiv");
+    form.appendChild(input);
+    let submitBtn = document.createElement("button");
+    submitBtn.setAttribute("class", "button12")
+    let btnValue = document.createTextNode("Add Task");
+    submitBtn.appendChild(btnValue);
 
-    newCard.appendChild(para);
+    form.appendChild(submitBtn);
+
+
+
+    newCard.appendChild(form);
     main.appendChild(newCard);
-
-
-
-
-
-
 
     const h3Value = newHeading.innerText;
 
+
+
+
     if (!Array.isArray(savedTasks[h3Value])) {
         savedTasks[h3Value] = [];
-        savedTasks[h3Value].push(valueTextArea);
+        savedTasks[h3Value].push();
     }
+    // console.log(createPara());
+
     localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
+
+
+    submitBtn.addEventListener("click", createTicket);
 
 
     deleteBtn.addEventListener("click", (e) => {
@@ -114,6 +152,13 @@ let createCard = (value, valueTextArea) => {
 
     })
 
+
+
+
+    // edit div value function
+    // edit div value function 
+    // edit div value function
+
     UpdateDiv.addEventListener("click", (e) => {
         e.preventDefault();
 
@@ -123,47 +168,54 @@ let createCard = (value, valueTextArea) => {
         let textAreaValue = e.target.parentElement.parentElement.parentElement.parentElement.children[1];
         let addValue = e.target.parentElement.parentElement.parentElement.parentElement;
 
-        
-        
+
+
         let targetChildren = getValue.children[0];
         targetChildren.remove();
-        
+
         let form = document.createElement("form");
         let updateInput = document.createElement("input");
         updateInput.setAttribute("class", "styling")
         updateInput.setAttribute("type", "text")
         updateInput.setAttribute("value", `${targetChildren.innerText}`);
         form.appendChild(updateInput);
-        
-        
+
+
         let UpdateTextArea = document.createElement("textarea");
         let createValueTextArea = document.createTextNode(textAreaValue.innerText)
         UpdateTextArea.appendChild(createValueTextArea);
-        
+
         form.appendChild(UpdateTextArea);
-        
+
         let submitBtn = document.createElement("button");
         let buttonValue = document.createTextNode("Update Note");
         submitBtn.setAttribute("class", "button1")
         let br = document.createElement("br");
-        
+
         form.appendChild(br)
         submitBtn.setAttribute("type", "submit");
         submitBtn.appendChild(buttonValue);
-        
+
         form.appendChild(submitBtn);
         textAreaValue.remove();
         getValue.appendChild(form)
-        
+
         let updted1 = document.createElement("input");
-        
+
         removeEle.remove();
     })
 }
 for (const test in savedTasks) {
     let showValue = savedTasks[test][0];
     let showHeading = test;
-    
+
     createCard(showHeading, showValue);
-    
+
 }
+
+for (const keyys of savedTasks[getValueHeading]){
+    createTicket();
+    console.log(keyys);
+}
+
+console.log(createTicket());
